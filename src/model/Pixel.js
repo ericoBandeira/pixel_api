b5f50db11dcae3f77718c5213f84371b77f851b7
Lucas Vieira <lucas.engen@outlook.com>
2022-02-11 01:23:43 -0300

    feat: :sparkles: implements associations - removes useless fields from model

--- src/model/Pixel.js
+++ src/model/Pixel.js
@@ -4,14 +4,9 @@ class Pixel extends Model {
   static init(sequelize) {
     super.init(
       {
-        id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        name: { type: DataTypes.TEXT, allowNull: false },
-        eye: { type: DataTypes.TEXT, allowNull: false },
+        id: { type: DataTypes.INTEGER },
+        name: { type: DataTypes.TEXT },
+        eye: { type: DataTypes.TEXT },
       },
       {
         sequelize,
@@ -20,7 +15,11 @@ class Pixel extends Model {
     );
   }
 
-  static associate(models) {}
+  static associate(models) {
+    this.belongsTo(models.Team, { foreignKey: "team_id" });
+    this.hasMany(models.Feature, { foreignKey: "pixel_id", as: "pixel_features" });
+    this.belongsTo(models.User, { foreignKey: "user_id" });
+  }
 }
 
 module.exports = Pixel;
