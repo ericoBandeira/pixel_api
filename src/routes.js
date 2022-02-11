054a8f3279368794688bfb572eace48777bce82f
Lucas Vieira <lucas.engen@outlook.com>
2022-02-11 01:21:45 -0300

    feat: :sparkles: adds routes to create/login user - adds route to return server health

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
+const UserController = require("./controller/UserController");
+const HeartbeatController = require("./controller/HeartbeatController");
+const jwt = require("./jwt");
 
 const routes = express.Router();
 
-routes.get("/");
+routes.get("/health", HeartbeatController.sendHealth);
+routes.post("/login", jwt.verifyJWT, UserController.loginUser);
+routes.post("/register", UserController.createUser);
 
 module.exports = routes;
