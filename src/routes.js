6f0e69f8b839422dbb72588f8567f086d747dada
Lucas Vieira <lucas.engen@outlook.com>
2022-02-13 06:45:16 -0300

    feat: adds routes to create/get pixels

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserController = require("./controller/UserController");
 const HeartbeatController = require("./controller/HeartbeatController");
+const PixelController = require("./controller/PixelController");
 const jwt = require("./jwt");
 
 const routes = express.Router();
@@ -8,5 +9,8 @@ const routes = express.Router();
 routes.get("/health", HeartbeatController.sendHealth);
 routes.post("/login", jwt.verifyJWT, UserController.loginUser);
 routes.post("/register", UserController.createUser);
+routes.post("/pixel/new", PixelController.createPixel);
+routes.get("/pixel/:name", PixelController.getPixelByName);
+routes.post("/pixel/:id/feed", PixelController.feedPixel);
 
 module.exports = routes;
