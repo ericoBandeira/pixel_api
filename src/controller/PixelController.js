db63e01a915319793de9010853831d7ad1bad027
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-02-14 00:49:36 -0300

    feat: :sparkles: adds method to change feature status - improves method to feed pixel

--- src/controller/PixelController.js
+++ src/controller/PixelController.js
@@ -20,15 +20,6 @@ async function getPixelByName(req, res) {
   return res.status(200).json(pixel);
 }
 
-async function createFeature(name, value) {
-  return await Feature.create({
-    name,
-    value,
-    active: true,
-    feeding_date: new Date(),
-  });
-}
-
 async function feedPixel(req, res) {
   const { id } = req.params;
   const {
@@ -47,33 +38,81 @@ async function feedPixel(req, res) {
     return res.status(404).json({ error: "pixel not found" });
   }
 
-  const visibility_feat = await Feature.findOne({ where: { name: "visibility" } });
+  const visibility_feat = await Feature.findOne({
+    where: { name: "visibility" },
+  });
   const match_feat = await Feature.findOne({ where: { name: "match" } });
   const control_feat = await Feature.findOne({ where: { name: "control" } });
-  const consistence_feat = await Feature.findOne({ where: { name: "consistence" } });
-  const recognition_feat = await Feature.findOne({ where: { name: "recognition" } });
-  const efficiency_feat = await Feature.findOne({ where: { name: "efficiency" } });
-  const minimalism_feat = await Feature.findOne({ where: { name: "minimalism" } });
+  const consistence_feat = await Feature.findOne({
+    where: { name: "consistence" },
+  });
+  const recognition_feat = await Feature.findOne({
+    where: { name: "recognition" },
+  });
+  const efficiency_feat = await Feature.findOne({
+    where: { name: "efficiency" },
+  });
+  const minimalism_feat = await Feature.findOne({
+    where: { name: "minimalism" },
+  });
   const error_prevention_feat = await Feature.findOne({
     where: { name: "error_prevention" },
   });
 
   visibility_feat.value = visibility;
+  visibility_feat.feeding_date = new Date();
   match_feat.value = match;
+  match_feat.feeding_date = new Date();
   control_feat.value = control;
+  control_feat.feeding_date = new Date();
   consistence_feat.value = consistence;
+  consistence_feat.feeding_date = new Date();
   recognition_feat.value = recognition;
+  recognition_feat.feeding_date = new Date();
   efficiency_feat.value = efficiency;
+  efficiency_feat.feeding_date = new Date();
   minimalism_feat.value = minimalism;
-  error_prevention.value = error_prevention;
+  minimalism_feat.feeding_date = new Date();
+  error_prevention_feat.value = error_prevention;
+  error_prevention_feat.feeding_date = new Date();
 
-  visibility_feat.setPixel(pixel);
+  pixel.setFeatures([
+    visibility_feat,
+    match_feat,
+    control_feat,
+    consistence_feat,
+    recognition_feat,
+    efficiency_feat,
+    minimalism_feat,
+    error_prevention_feat,
+  ]);
 
   return res.status(200).json(response);
+}
+
+async function enablePixelFeature(req, res) {
+  const { id, feat_id } = req.params;
+  const { enabled } = req.body;
+
+  const pixel = await Pixel.findByPk(id);
+  const feature = await Feature.findByPk(feat_id);
+  if (!pixel || !feature) {
+    return res.status(404).json({ error: "pixel or feature not found" });
+  }
+
+  feature.active = !!enabled;
+
+  pixel.addFeature(feature);
+
+  // Get features of pixel
+  const features = await Pixel.getFeatures();
+
+  return res.status(200).json(features);
 }
 
 module.exports = {
   createPixel,
   getPixelByName,
   feedPixel,
+  enablePixelFeature,
 };
