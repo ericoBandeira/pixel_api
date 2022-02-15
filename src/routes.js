a4fba167198ddab643994b00310c4402fcdc803f
Lucas Vieira <lucas.engen@outlook.com>
2022-02-15 12:16:49 -0300

    chore: removes token from login route

--- src/routes.js
+++ src/routes.js
@@ -7,17 +7,14 @@ const jwt = require("./jwt");
 
 const routes = express.Router();
 
-routes.get("/health", HeartbeatController.sendHealth);
-routes.post("/login", jwt.verifyJWT, UserController.loginUser);
+routes.get("/health", jwt.verifyJWT, HeartbeatController.sendHealth);
+routes.post("/login", UserController.loginUser);
 routes.post("/register", UserController.createUser);
 routes.post("/pixel/new", PixelController.createPixel);
 routes.get("/pixel/:name", PixelController.getPixelByName);
 routes.get("/pixel/by-mail/:mail", UserController.findPixelByUser);
 routes.post("/pixel/:id/feed", PixelController.feedPixel);
 routes.get("/features", FeatureController.getFeatures);
-routes.post(
-  "/pixel/:id/feature/:feat_id/enable",
-  PixelController.enablePixelFeature
-);
+routes.post("/pixel/:id/feature/:feat_id/enable", PixelController.enablePixelFeature);
 
 module.exports = routes;
