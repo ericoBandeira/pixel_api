7ca9537ba0452f2355e3b9d353329e6f1615a91b
Lucas Vieira <lucas.engen@outlook.com>
2022-02-15 22:39:35 -0300

    feat: :sparkles: adds methods to handle teams

--- src/controller/TeamController.js
+++ src/controller/TeamController.js
@@ -1,3 +1,4 @@
+const Pixel = require("../model/Pixel");
 const Team = require("../model/Team");
 
 async function createTeam(req, res) {
@@ -6,4 +7,46 @@ async function createTeam(req, res) {
   return res.status(200).json(team);
 }
 
-module.exports = { createTeam };
+async function getTeamPixel(req, res) {
+  const { team_id } = req.params;
+
+  const team = await Team.findByPk(team_id);
+  if (!team) {
+    return res.status(404).json({ error: "team not found" });
+  }
+
+  const pixel = await team.getPixel();
+  if (!pixel) {
+    return res
+      .status(200)
+      .json({ message: "there's no pixel associated with this team" });
+  }
+
+  return res.status(200).json(pixel);
+}
+
+async function setTeamPixel(req, res) {
+  const { pixel_id, team_id } = req.params;
+
+  const pixel = await Pixel.findByPk(pixel_id);
+  const team = await Team.findByPk(team_id);
+  if (!pixel || !team) {
+    return res.status(404).json({ error: "pixel or team not found" });
+  }
+
+  const currentPixel = await team.getPixel();
+  if (currentPixel) {
+    return res.status(400).json({ error: "this team already has a pixel" });
+  }
+
+  const teamPixel = await team.setPixel(pixel);
+  await team.save();
+
+  return res.status(200).json(teamPixel);
+}
+
+async function getTeams(req, res) {
+  const teams = await Team.findAll();
+  return res.status(200).json(teams);
+}
+module.exports = { createTeam, getTeamPixel, setTeamPixel, getTeams };
