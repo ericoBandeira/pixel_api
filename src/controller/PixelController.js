4384cb3c5eafed07d7236e891a19f8f3ebd04dc6
Lucas Vieira <lucas.engen@outlook.com>
2022-02-15 21:25:54 -0300

    fix: fixes method used to change property value - fixes method used to enable property

--- src/controller/PixelController.js
+++ src/controller/PixelController.js
@@ -2,59 +2,7 @@ const Pixel = require("../model/Pixel");
 const Feature = require("../model/Feature");
 const User = require("../model/User");
 
-async function createPixel(req, res) {
-  const { name, eye } = req.body;
-
-  const userId = req.decoded.user_id;
-
-  if (!name || !eye) {
-    return res.status(400).json({ error: "missing required fields in body" });
-  }
-
-  const newPixel = await Pixel.create({ name, eye });
-  if (!newPixel) {
-    return res.status(500).json({ error: "could not create pixel" });
-  }
-
-  const user = await User.findByPk(userId);
-  if (!user) {
-    return res.status(404).json({ error: "user not found" });
-  }
-
-  const userPixel = await user.setPixel(newPixel);
-
-  return res.status(200).json(userPixel);
-}
-
-async function getPixelByName(req, res) {
-  const { name } = req.params;
-
-  const pixel = await Pixel.findOne({ where: { name } });
-  if (!pixel) {
-    return res.status(404).json({ error: `pixel not found: ${name}` });
-  }
-
-  return res.status(200).json(pixel);
-}
-
-async function feedPixel(req, res) {
-  const { id } = req.params;
-  const {
-    visibility,
-    match,
-    control,
-    consistence,
-    recognition,
-    efficiency,
-    minimalism,
-    error_prevention,
-  } = req.body;
-
-  const pixel = await Pixel.findByPk(id);
-  if (!pixel) {
-    return res.status(404).json({ error: "pixel not found" });
-  }
-
+async function addAllFeatures(pixel) {
   const visibility_feat = await Feature.findOne({
     where: { name: "visibility" },
   });
@@ -76,60 +24,112 @@ async function feedPixel(req, res) {
     where: { name: "error_prevention" },
   });
 
-  visibility_feat.value = visibility;
-  visibility_feat.feeding_date = new Date();
-  match_feat.value = match;
-  match_feat.feeding_date = new Date();
-  control_feat.value = control;
-  control_feat.feeding_date = new Date();
-  consistence_feat.value = consistence;
-  consistence_feat.feeding_date = new Date();
-  recognition_feat.value = recognition;
-  recognition_feat.feeding_date = new Date();
-  efficiency_feat.value = efficiency;
-  efficiency_feat.feeding_date = new Date();
-  minimalism_feat.value = minimalism;
-  minimalism_feat.feeding_date = new Date();
-  error_prevention_feat.value = error_prevention;
-  error_prevention_feat.feeding_date = new Date();
+  await pixel.addFeature(visibility_feat);
+  await pixel.addFeature(match_feat);
+  await pixel.addFeature(control_feat);
+  await pixel.addFeature(consistence_feat);
+  await pixel.addFeature(recognition_feat);
+  await pixel.addFeature(efficiency_feat);
+  await pixel.addFeature(minimalism_feat);
+  await pixel.addFeature(error_prevention_feat);
+}
 
-  pixel.setFeatures([
-    visibility_feat,
-    match_feat,
-    control_feat,
-    consistence_feat,
-    recognition_feat,
-    efficiency_feat,
-    minimalism_feat,
-    error_prevention_feat,
-  ]);
+async function createPixel(req, res) {
+  const { name, eye } = req.body;
 
-  return res.status(200).json(response);
+  const userId = req.decoded.user_id;
+
+  if (!name || !eye) {
+    return res.status(400).json({ error: "missing required fields in body" });
+  }
+
+  const newPixel = await Pixel.create({ name, eye });
+  if (!newPixel) {
+    return res.status(500).json({ error: "could not create pixel" });
+  }
+
+  const user = await User.findByPk(userId);
+  if (!user) {
+    return res.status(404).json({ error: "user not found" });
+  }
+
+  const userPixel = await user.setPixel(newPixel);
+
+  // Add features to the created pixel
+  await addAllFeatures(userPixel);
+
+  return res.status(200).json(userPixel);
+}
+
+async function feedPixel(req, res) {
+  const { id } = req.params;
+  const {
+    visibility,
+    match,
+    control,
+    consistence,
+    recognition,
+    efficiency,
+    minimalism,
+    error_prevention,
+  } = req.body;
+
+  const pixel = await Pixel.findByPk(id);
+  if (!pixel) {
+    return res.status(404).json({ error: "pixel not found" });
+  }
+
+  const features = await pixel.getFeatures();
+
+  features.forEach(async (feat) => {
+    if (feat.name === "visibility") {
+      feat.value = visibility ? visibility : feat.value;
+    } else if (feat.name === "match") {
+      feat.value = match ? match : feat.value;
+    } else if (feat.name === "control") {
+      feat.value = control ? control : feat.value;
+    } else if (feat.name === "consistence") {
+      feat.value = consistence ? consistence : feat.value;
+    } else if (feat.name === "recognition") {
+      feat.value = recognition ? recognition : feat.value;
+    } else if (feat.name === "efficiency") {
+      feat.value = efficiency ? efficiency : feat.value;
+    } else if (feat.name === "minimalism") {
+      feat.value = minimalism ? minimalism : feat.value;
+    } else if (feat.name === "error_prevention") {
+      feat.value = error_prevention ? error_prevention : feat.value;
+    }
+
+    await feat.save();
+  });
+
+  return res.status(200).json(features);
 }
 
 async function enablePixelFeature(req, res) {
   const { id, feat_id } = req.params;
   const { enabled } = req.body;
 
+  const featId = Number.parseInt(feat_id);
+
   const pixel = await Pixel.findByPk(id);
-  const feature = await Feature.findByPk(feat_id);
-  if (!pixel || !feature) {
+  const features = await pixel.getFeatures();
+  if (!pixel || !features) {
     return res.status(404).json({ error: "pixel or feature not found" });
   }
 
-  feature.active = !!enabled;
-
-  pixel.addFeature(feature);
-
-  // Get features of pixel
-  const features = await Pixel.getFeatures();
+  features.forEach(async (feature) => {
+    if (feature.id === featId) {
+      feature.active = !!enabled;
+      await feature.save();
+    }
+  });
 
   return res.status(200).json(features);
 }
 
 module.exports = {
   createPixel,
-  getPixelByName,
   feedPixel,
   enablePixelFeature,
 };
