61c02dd474894b09c7f87ba86fdb12a23244de65
Lucas Vieira <lucas.engen@outlook.com>
2022-02-17 23:12:22 -0300

    chore: omits some fields in features endpoint

--- src/controller/FeatureController.js
+++ src/controller/FeatureController.js
@@ -2,7 +2,9 @@ const Feature = require("../model/Feature");
 
 async function getFeatures(req, res) {
   try {
-    const features = await Feature.findAll();
+    const features = await Feature.findAll({
+      attributes: ["id", "name", "value", "active", "feeding_date"],
+    });
 
     return res.status(200).json(features);
   } catch (err) {
